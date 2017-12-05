d8076dc302e489f7e9d225d063756e76650a7441
nasiryare206 <nasiryare206@gmail.com>
2017-12-05 05:11:32 -0800

    Create surveyform.php

---
+++ surveyform.php
@@ -0,0 +1,23 @@
+<h1>Thank you for completing the survey</h1>
+
+<h2>Your responses are: </h2>
+
+<?php
+     	
+	
+	
+	$firstname = $_POST['firstname'];
+	$age  = $_POST['age'];
+	$gender  = $_POST['gender'];
+	$grade  = $_POST['grade'];
+	$sports = $_POST['sports'];
+	
+	
+    echo "<h4> Your full name is $firstname</h4>";
+	echo "<h4>Your age is $age </h4>";
+	echo "<h4> You are a $gender</h4>";
+	echo "<h4> Your grade in this class is $grade</h4>";
+	echo "<h4> Your favorite sport is $sports</h4>";
+	
+     
+?>
